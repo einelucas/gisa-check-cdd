0b311684dbfec5eda3df9891076567f800affd55
einelucas <lucaspinheiro863@gmail.com>
2026-01-14 10:42:57 -0400

    att

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/gisa-check-cdd/", // 🔒 garante assets corretos no GitHub Pages
+  base: "/gisa-check-cdd/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
